7fa56491573a55b7e77a38c66c958dfc925ce9aa
ArianeDucellier <ariane.ducellier.pro@gmail.com>
2018-07-06 15:54:06 -0700

    Write report

--- catalog/compute_templates.py
+++ catalog/compute_templates.py
@@ -1,7 +1,7 @@
 """
 This module contains a function to download every one-minute time window
-where there is an LFE recorded, stack the signal over all the LFEs, and
-compare the waveform with the one from Plourde et al. (2015)
+where there is an LFE recorded, stack the signal over all the LFEs, cross
+coorelate each window with the stack, sort the LFEs and keep only the best
 """
 
 import obspy
@@ -10,13 +10,15 @@ from obspy import read
 from obspy import read_inventory
 from obspy import UTCDateTime
 from obspy.core.stream import Stream
+from obspy.signal.cross_correlation import correlate
 
+import matplotlib.cm as cm
 import matplotlib.pyplot as plt
 import numpy as np
 import os
+import pickle
 
 from fractions import Fraction
-from scipy.io import loadmat
 
 from stacking import linstack
 
@@ -140,10 +142,11 @@ def get_from_NCEDC(station, Tstart, Tend, filt, dt):
         D.decimate(ratio.numerator, no_filter=True)
         return(D)
 
-def get_waveform(filename, TDUR, filt, method='RMS'):
+def compute_templates(filename, TDUR, filt, ratios, dt, ncor, method='RMS'):
     """
-    This function computes the waveform for each template and compare it to
-    the waveform from Plourde et al. (2015)
+    This function computes the waveform for each template, cross correlate
+    them with the stack, and keep only the best to get the final template
+    that will be used to find LFEs
 
     Input:
         type filename = string
@@ -152,6 +155,12 @@ def get_waveform(filename, TDUR, filt, method='RMS'):
         TDUR = Time to add before and after the time window for tapering
         type filt = tuple of floats
         filt = Lower and upper frequencies of the filter
+        type ratios = list of floats
+        ratios = Percentage of LFEs to be kept for the final template
+        type dt = float
+        dt = Time step for resampling
+        type ncor = integer
+        ncor = Number of points for the cross correlation
         type method = string
         method = Normalization method for linear stack (RMS or Max)
     Output:
@@ -171,15 +180,8 @@ def get_waveform(filename, TDUR, filt, method='RMS'):
              'formats': (np.float, '|S6', np.int, np.float, np.float)}, \
         skiprows=2)
 
-    # Get the waveforms from the catalog of Plourde et al. (2015)
-    data = loadmat('../data/LFEcatalog/waveforms/' + filename + '.mat')
-    ndt = ndt = data['ndt'][0][0]
-    ordlst = data['ordlst']
-    uk = data['uk']
-    ns = len(ordlst)
-
     # Create directory to store the waveforms
-    namedir = 'waveforms/' + filename
+    namedir = 'templates/' + filename
     if not os.path.exists(namedir):
         os.makedirs(namedir)
 
@@ -207,10 +209,10 @@ def get_waveform(filename, TDUR, filt, method='RMS'):
             Tend = Tori + 60.0 + TDUR
             # First case: we can get the data from IRIS
             if (station[0 : 2] == 'ME'):
-                D = get_from_IRIS(station, Tstart, Tend, filt, ndt)
+                D = get_from_IRIS(station, Tstart, Tend, filt, dt)
             # Second case: we get the data from NCEDC
             else:
-                D = get_from_NCEDC(station, Tstart, Tend, filt, ndt)
+                D = get_from_NCEDC(station, Tstart, Tend, filt, dt)
             if (type(D) == obspy.core.stream.Stream):
                 # Add to stream
                 if (station == 'B039'):
@@ -229,134 +231,106 @@ def get_waveform(filename, TDUR, filt, method='RMS'):
                         Tori + 60.0)[0])
             else:
                 print('Failed at downloading data')
-        # Stack and plot
-        if (len(EW) > 0 and len(NS) > 0 and len(UD) > 0):
+        # Stack
+        if (len(EW) > 0):
             # Stack waveforms
             EWstack = linstack([EW], normalize=True, method=method) 
             NSstack = linstack([NS], normalize=True, method=method)
             UDstack = linstack([UD], normalize=True, method=method)
-            # First figure
-            # Comparison with the waveforms from Plourde et al. (2015)
+            # Cross correlation
+            maxCC = np.zeros(len(EW))
+            for i in range(0, len(EW)):
+                ccEW = correlate(EW[i], EWstack[0], ncor)
+                ccNS = correlate(NS[i], NSstack[0], ncor)
+                ccUD = correlate(UD[i], UDstack[0], ncor)
+                maxCC[i] = np.max(ccEW) + np.max(ccNS) + np.max(ccUD)
+            # Sort cross correlations
+            index = np.flip(np.argsort(maxCC), axis=0)
+            EWbest = Stream()
+            NSbest = Stream()
+            UDbest = Stream()
+            # Compute stack of best LFEs
+            for j in range(0, len(ratios)):
+                nLFE = int(ratios[j] * len(EW) / 100.0)
+                EWselect = Stream()
+                NSselect = Stream()
+                UDselect = Stream()
+                for i in range(0, nLFE):
+                    EWselect.append(EW[index[i]])
+                    NSselect.append(NS[index[i]])
+                    UDselect.append(UD[index[i]])
+                # Stack best LFEs
+                EWbest.append(linstack([EWselect], normalize=True, \
+                    method=method)[0])
+                NSbest.append(linstack([NSselect], normalize=True, \
+                    method=method)[0])
+                UDbest.append(linstack([UDselect], normalize=True, \
+                    method=method)[0])
+            # Plot figure
             plt.figure(1, figsize=(20, 15))
-            station4 = station
-            if (len(station4) < 4):
-                for j in range(len(station), 4):
-                    station4 = station4 + ' '
-            index = np.argwhere(ordlst == station4)[0][0]
-            # EW component
+            colors = cm.rainbow(np.linspace(0, 1, len(ratios)))
+            # East - West component
             ax1 = plt.subplot(311)
             dt = EWstack[0].stats.delta
             nt = EWstack[0].stats.npts
             t = dt * np.arange(0, nt)
-            plt.plot(t, EWstack[0].data, 'r', label='Stack')
-            t0 = ndt * np.arange(0, np.shape(uk)[1])
-            plt.plot(t0, uk[ns + index, :], 'k', label='Waveform')
-            plt.xlim(0.0, 60.0)
-            plt.title('East component', fontsize=16)
-            plt.xlabel('Time (s)', fontsize=16)
-            plt.ylabel('Velocity (m/s)', fontsize=16)
+            plt.plot(t, EWstack[0].data, 'k', label='All')
+            for j in range(0, len(ratios)):
+                plt.plot(t, EWbest[j].data, color = colors[j], \
+                    label = str(int(ratios[j])) + '%')
+            plt.xlim([np.min(t), np.max(t)])
+            plt.title('East - West component', fontsize=20)
+            plt.xlabel('Time (s)', fontsize=20)
             plt.legend(loc=1)
-            # NS component
+            # North - South component
             ax2 = plt.subplot(312)
             dt = NSstack[0].stats.delta
             nt = NSstack[0].stats.npts
             t = dt * np.arange(0, nt)
-            plt.plot(t, NSstack[0].data, 'r', label='Stack')
-            t0 = ndt * np.arange(0, np.shape(uk)[1])
-            plt.plot(t0, uk[index, :], 'k', label='Waveform')
-            plt.xlim(0.0, 60.0)
-            plt.title('North component', fontsize=16)
-            plt.xlabel('Time (s)', fontsize=16)
-            plt.ylabel('Velocity (m/s)', fontsize=16)
+            plt.plot(t, NSstack[0].data, 'k', label='All')
+            for j in range(0, len(ratios)):
+                plt.plot(t, NSbest[j].data, color = colors[j], \
+                    label = str(int(ratios[j])) + '%')
+            plt.xlim([np.min(t), np.max(t)])
+            plt.title('North - South component', fontsize=20)
+            plt.xlabel('Time (s)', fontsize=20)
             plt.legend(loc=1)
-            # UD component
+            # Vertical component
             ax3 = plt.subplot(313)
             dt = UDstack[0].stats.delta
             nt = UDstack[0].stats.npts
             t = dt * np.arange(0, nt)
-            plt.plot(t, UDstack[0].data, 'r', label='Stack')
-            t0 = ndt * np.arange(0, np.shape(uk)[1])
-            plt.plot(t0, uk[2 * ns + index, :], 'k', label='Waveform')
-            plt.xlim(0.0, 60.0)
-            plt.title('Vertical component', fontsize=16)
-            plt.xlabel('Time (s)', fontsize=16)
-            plt.ylabel('Velocity (m/s)', fontsize=16)
+            plt.plot(t, UDstack[0].data, 'k', label='All')
+            for j in range(0, len(ratios)):
+                plt.plot(t, UDbest[j].data, color = colors[j], \
+                    label = str(int(ratios[j])) + '%')
+            plt.xlim([np.min(t), np.max(t)])
+            plt.title('Vertical component', fontsize=20)
+            plt.xlabel('Time (s)', fontsize=20)
             plt.legend(loc=1)
-            # End and save figure
+            # End figure
             plt.suptitle(station, fontsize=24)
-            plt.savefig(namedir + '/' + station + '_compare.eps', format='eps')
+            plt.savefig(namedir + '/' + station + '.eps', format='eps')
             ax1.clear()
             ax2.clear()
             ax3.clear()
             plt.close(1)
-            # Second figure
-            # Look at all the waveforms and the stack
-            plt.figure(2, figsize=(30, 15))
-            # EW component
-            ax1 = plt.subplot(131)
-            for i in range(0, len(EW)):
-                dt = EW[i].stats.delta
-                nt = EW[i].stats.npts
-                t = dt * np.arange(0, nt)
-                datanorm = EW[i].data / np.max(np.abs(EW[i].data))
-                plt.plot(t, (2.0 * i + 1) + datanorm, 'k-')
-            datanorm = EWstack[0].data / np.max(np.abs(EWstack[0].data))
-            plt.plot(t, - 2.0 + datanorm, 'r-')
-            plt.xlim(0.0, 60.0)
-            plt.ylim(- 3.0, 2.0 * len(EW))
-            plt.title('East component', fontsize=24)
-            plt.xlabel('Time (s)', fontsize=24)
-            plt.ylabel('Velocity (m/s)', fontsize=24)
-            ax1.set_yticklabels([])
-            ax1.tick_params(labelsize=20)
-            # NS component
-            ax2 = plt.subplot(132)
-            for i in range(0, len(NS)):
-                dt = NS[i].stats.delta
-                nt = NS[i].stats.npts
-                t = dt * np.arange(0, nt)
-                datanorm = NS[i].data / np.max(np.abs(NS[i].data))
-                plt.plot(t, (2.0 * i + 1) + datanorm, 'k-')
-            datanorm = NSstack[0].data / np.max(np.abs(NSstack[0].data))
-            plt.plot(t, - 2.0 + datanorm, 'r-')
-            plt.xlim(0.0, 60.0)
-            plt.ylim(- 3.0, 2.0 * len(NS))
-            plt.title('North component', fontsize=24)
-            plt.xlabel('Time (s)', fontsize=24)
-            plt.ylabel('Velocity (m/s)', fontsize=24)
-            ax2.set_yticklabels([])
-            ax2.tick_params(labelsize=20)
-            # UD component
-            ax3 = plt.subplot(133)
-            for i in range(0, len(UD)):
-                dt = UD[i].stats.delta
-                nt = UD[i].stats.npts
-                t = dt * np.arange(0, nt)
-                datanorm = UD[i].data / np.max(np.abs(UD[i].data))
-                plt.plot(t, (2.0 * i + 1) + datanorm, 'k-')  
-            datanorm = UDstack[0].data / np.max(np.abs(UDstack[0].data))
-            plt.plot(t, - 2.0 + datanorm, 'r-')
-            plt.xlim(0.0, 60.0)
-            plt.ylim(- 3.0, 2.0 * len(UD))
-            plt.title('Vertical component', fontsize=24)
-            plt.xlabel('Time (s)', fontsize=24)
-            plt.ylabel('Velocity (m/s)', fontsize=24)
-            ax3.set_yticklabels([])
-            ax3.tick_params(labelsize=20)
-            # End and save figure
-            plt.suptitle(station, fontsize=24)
-            plt.savefig(namedir + '/' + station + '_stack.eps', format='eps')
-            ax1.clear()
-            ax2.clear()
-            ax3.clear()
-            plt.close(2)               
+            # Save stacks into file
+            for j in range(0, len(ratios)):
+                filename = namedir + '/' + station + '_' + \
+                    str(int(ratios[j])) + '.pkl'
+                pickle.dump([EWbest[j], NSbest[j], UDbest[j]], \
+                    open(filename, 'wb'))
 
 if __name__ == '__main__':
 
     # Set the parameters
-    filename = '080413.18.013'
+    filename = '080405.11.042'
     TDUR = 10.0
     filt = (1.5, 9.0)
+    ratios = [50.0, 60.0, 70.0, 80.0, 90.0]
+    dt = 0.05
+    ncor = 400
     method = 'RMS'
 
-    get_waveform(filename, TDUR, filt, method)
+    compute_templates(filename, TDUR, filt, ratios, dt, ncor, method)
