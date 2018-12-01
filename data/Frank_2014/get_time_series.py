c74a2cf2e341b03ceb2bb05b4100dffc7d5d1714
ArianeDucellier <ariane.ducellier.pro@gmail.com>
2018-11-30 16:37:31 -0800

    Clean repository

---
+++ data/Frank_2014/get_time_series.py
@@ -0,0 +1,82 @@
+"""
+This script looks at the catalog from Frank et al. (2014) and transform
+the LFE detections for each family into a time series
+"""
+
+import numpy as np
+import pandas as pd
+import pickle
+
+from datetime import datetime, timedelta
+from math import floor
+
+def get_time_series(LFEtime, filename, window, tbegin, tend):
+    """
+    Function to transform the LFE catalog into a time series
+
+    Input:
+        type LFEtime = panda DataFrame
+        LFEtime = Times of LFE detections
+        type filename = int
+        filename = Name of the LFE family
+        type window = float
+        window = Duration of the time window where we count the number of LFEs
+                 (in seconds)
+        type tbegin = obspy UTCDateTime
+        tbegin = Beginning time of the catalog 
+        type tend = obspy UTCDateTime
+        tend = End time of the catalog
+    Output:
+        type X = numpy array
+        X = Time series with number of LFEs per time window
+    """
+    # Length of the time series
+    dt = tend - tbegin
+    duration = dt.days * 86400.0 + dt.seconds + dt.microseconds * 0.000001
+    nw = int(duration / window)
+    # Get the data for the family
+    data = LFEtime.loc[LFEtime['ID'] == filename]
+    X = np.zeros(nw, dtype=int)
+    # Loop on LFEs
+    for j in range(0, data.shape[0]):
+        myYear = data['year'].iloc[j]
+        myMonth = data['month'].iloc[j]
+        myDay = data['day'].iloc[j]
+        myHour = data['hour'].iloc[j]
+        myMinute = data['minute'].iloc[j]
+        mySecond = int(floor(data['second'].iloc[j]))
+        myMicrosecond = int(1000000.0 * (data['second'].iloc[j] - mySecond))
+        t = datetime(myYear, myMonth, myDay, myHour, myMinute, mySecond, \
+            myMicrosecond)
+        # Add LFE to appropriate time window
+        if ((tbegin <= t) and (t < tbegin + timedelta(seconds=nw * window))):
+            dt = t - tbegin
+            duration = dt.days * 86400.0 + dt.seconds + dt.microseconds * \
+                0.000001
+            index = int(duration / window)
+            X[index] = X[index] + 1
+    return X
+
+if __name__ == '__main__':
+
+    # Read the LFE file
+    LFEtime = pd.read_csv('frank_jgr_2014_lfe_catalog.txt', \
+        delim_whitespace=True, header=None, skiprows=0)
+    LFEtime.columns = ['year', 'month', 'day', 'hour', 'minute', 'second', \
+        'ID', 'latitude', 'longitude', 'depth']
+    LFEtime['ID'] = LFEtime.ID.astype('category')
+    families = LFEtime['ID'].cat.categories.tolist()
+
+    # Beginning and end of the period we are looking at
+    tbegin = datetime(2005, 1, 15, 0, 0, 0)
+    tend = datetime(2007, 4, 16, 0, 0, 0)
+
+    # We construct the time series by counting the number of LFEs
+    # per one-day-long time window
+    window = 60.0
+
+    # Loop on LFE families
+    for i in range(0, len(families)):
+        X = get_time_series(LFEtime, families[i], window, tbegin, tend)
+        output = 'timeseries/{}.pkl'.format(str(families[i]))
+        pickle.dump([window, tbegin, tend, X], open(output, 'wb'))
---
+++ data/Frank_2014/plot_daily_LFEs.py
@@ -0,0 +1,65 @@
+"""
+This script looks at the catalog from Frank et al. (2014) and plots for
+each family the daily number of LFEs in function of time
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+from datetime import datetime, timedelta
+from math import floor
+
+# Read the LFE file
+LFEtime = pd.read_csv('frank_jgr_2014_lfe_catalog.txt', \
+    delim_whitespace=True, header=None, skiprows=0)
+LFEtime.columns = ['year', 'month', 'day', 'hour', 'minute', 'second', \
+    'ID', 'latitude', 'longitude', 'depth']
+LFEtime['ID'] = LFEtime.ID.astype('category')
+families = LFEtime['ID'].cat.categories.tolist()
+
+# Beginning and end of the period we are looking at
+tbegin = datetime(2005, 1, 15, 0, 0, 0)
+tend = datetime(2007, 4, 16, 0, 0, 0)
+
+# We construct the time series by counting the number of LFEs
+# per one-day-long time window
+window = 86400.0
+
+# Length of the time series
+dt = tend - tbegin
+duration = dt.days * 86400.0 + dt.seconds + dt.microseconds * 0.000001
+nw = int(duration / window)
+
+# Loop on LFE families
+for i in range(0, len(families)):
+    data = LFEtime.loc[LFEtime['ID'] == families[i]]
+    X = np.zeros(nw, dtype=int)
+    # Loop on LFEs
+    for j in range(0, data.shape[0]):
+        myYear = data['year'].iloc[j]
+        myMonth = data['month'].iloc[j]
+        myDay = data['day'].iloc[j]
+        myHour = data['hour'].iloc[j]
+        myMinute = data['minute'].iloc[j]
+        mySecond = int(floor(data['second'].iloc[j]))
+        myMicrosecond = int(1000000.0 * (data['second'].iloc[j] - mySecond))
+        t = datetime(myYear, myMonth, myDay, myHour, myMinute, mySecond, \
+            myMicrosecond)
+        # Add LFE to appropriate time window
+        if ((tbegin <= t) and (t < tbegin + timedelta(seconds=nw * window))):
+            dt = t - tbegin
+            duration = dt.days * 86400.0 + dt.seconds + dt.microseconds * \
+                0.000001
+            index = int(duration / window)
+            X[index] = X[index] + 1    
+    # Plot figure
+    plt.figure(1, figsize=(20, 10))
+    plt.stem(np.arange(0, len(X)), X, 'k-', markerfmt=' ', basefmt=' ')
+    plt.xlim([-0.5, len(X) - 0.5])
+    plt.xlabel('Time (days) since 2005/01/15', fontsize=24)
+    plt.ylabel('Number of LFEs', fontsize=24)
+    plt.title('Family {} ({:d} LFEs)'.format(str(families[i]), np.sum(X)), \
+        fontsize=24)
+    plt.savefig('LFEdistribution/' + str(families[i]) + '.eps', format='eps')
+    plt.close(1)
