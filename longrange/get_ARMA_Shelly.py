ce1dccb9c78c74261af49b2fafbb50e05c27b1d2
ArianeDucellier <ariane.ducellier@gmail.com>
2021-08-02 14:55:56 -0700

    ARMA model

--- longrange/get_ARMA_Shelly.py
+++ longrange/get_ARMA_Shelly.py
@@ -30,12 +30,13 @@ def aggregate(X, m):
     X2 = X[0 : N2 * m]
     X2 = np.reshape(X2, (N2, int(m)))
     Xm = np.sum(X2, axis=1)
-    return X
+    return Xm
 
 family_list = ['1788sss', '1812sss', '15025s', '25879s', '32192s', '41767s']
 p_list = [1, 1, 1, 1, 1, 1]
 
 h = 21
+alpha = 0.05
 
 dirname = '../data/Shelly_2017/timeseries/'
 
@@ -48,6 +49,7 @@ for (family, p) in zip(family_list, p_list):
     data = pickle.load(open(dirname + family + '.pkl', 'rb'))
     X = data[3]
     X = aggregate(X, 60 * 24)
+    X = X - np.mean(X)
     (phi, sigma2) = yule_walker(X, p)
     acvf = ACVF(X, h)
     acvf_hat = sigma2 / (1.0 - phi[0]**2.0) * np.power(np.repeat(phi[0], h), np.arange(0, h))
@@ -64,14 +66,26 @@ for (family, p) in zip(family_list, p_list):
     plt.savefig('Model/' + family + '.eps', format='eps')
     plt.close(1)
     # Plot ACVF of residuals
-    plt.figure(2, figsize=(9, 6))
+    plt.figure(2, figsize=(18, 6))
     N = np.shape(Z)[0]
     acf_Z = ACF(acvf_Z)
+    plt.subplot2grid((1, 2), (0, 0))
     plt.plot(np.arange(0, h), acf_Z, 'bo')
     plt.axhline(1.96 / sqrt(N), linewidth=2, linestyle='--', color='red')
     plt.axhline(-1.96 / sqrt(N), linewidth=2, linestyle='--', color='red')
     plt.xlabel('Time lag', fontsize=24)
     plt.ylabel('ACF', fontsize=24)
+    (QLB, quantiles) = portmanteau(acf_Z, N, alpha, p)
+    plt.subplot2grid((1, 2), (0, 1))
+    plt.plot(np.arange(p + 1, h), QLB[p : h], 'bo')
+    for j in range(p + 1, h):
+        if (j == p + 1):
+            plt.plot(np.linspace(j - 0.25, j + 0.25, 2), np.repeat(quantiles[j - 1], 2), 'r-', \
+                label='{} quantile'.format(100 * (1 - alpha)))
+        else:
+            plt.plot(np.linspace(j - 0.25, j + 0.25, 2), np.repeat(quantiles[j - 1], 2), 'r-')
+    plt.xlabel('Time lag', fontsize=24)
+    plt.ylabel('QLB', fontsize=24)
     plt.tight_layout()
     plt.savefig('Model/' + family + '_residuals.eps', format='eps')
     plt.close(2)
